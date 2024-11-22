d9493ea0cece4e99f741f7f03bd1627a65f89fc9
Dmitriy Bessarab <bessarabds@gmail.com>
2024-11-22 13:48:11 +0500

    to start discussion

---
+++ Markdown/Token.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown
+{
+    public class Token
+    {
+        public readonly int Position;
+      
+        public readonly string Value;
+
+        public readonly TokenProperty Property;
+
+        public Token(int position, string value)
+        {
+            Position = position;
+            Value = value;
+        }
+    }
+}
